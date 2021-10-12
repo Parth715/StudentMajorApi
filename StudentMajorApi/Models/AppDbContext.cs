09d2cd62e84eb225a877a1d0803e9a9744ad1d04
Parth Patel <parth2002715@gmail.com>
2021-10-11 20:02:44 -0400

    Add project files.

---
+++ StudentMajorApi/Models/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StudentMajorApi.Models
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Major> Majors { get; set; }
+        public DbSet<Student> Students { get; set; }
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+        protected override void OnModelCreating(ModelBuilder builder) { }
+    }
+}
---
+++ StudentMajorApi/Models/Student.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StudentMajorApi.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        [StringLength(30), Required]
+        public string Firstname { get; set; }
+        [StringLength(30), Required]
+        public string Lastname { get; set; }
+        [StringLength(2), Required]
+        public string StateCode { get; set; }
+        [Required]
+        public int Sat { get; set; }
+        [Column(TypeName = "decimal(1,2")]
+        public Decimal Gpa { get; set; }
+        public int MajorId { get; set; }
+        public virtual Major Major { get; set; }
+
+        public Student() { }
+    }
+}
